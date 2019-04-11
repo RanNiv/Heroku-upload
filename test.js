26637dcf8a087194e80e405121da4ae64e4cc85e
RanNiv <35506862+RanNiv@users.noreply.github.com>
2019-04-11 22:58:03 +0300

    update project

--- test.js
+++ test.js
@@ -1,27 +1,13 @@
-/* var express = require('express');
-var port = process.env.PORT || 3000;
-var app = express();
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Test: 'Express'}));
-});
-app.listen(port, function () {
- console.log(`Example app listening on port !`);
-}); */
+
+ 
+
+const express= require ('express');
+const app=express();
+app.use(express.static('./dist'));
+let port=process.env.PORT||3500;
+app.listen(port,()=>{console.log(`server listening on port
+${port}`)});
 
 
-var port=process.env.port || 8081;
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-   res.send('Test Node js');
-})
-
-var server = app.listen(port, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example node app listening at http://%s:%s", host, port)
-})
 
 
